ddbd85406640f2532249eef5b8eb7d4ffa2fabbb
hania <hania.rudy@gmail.com>
2017-04-01 16:37:08 +0200

    use ngRepeat directive for looping

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-// Changing scope value async; does not propagate without apply
+// Loopingw ith engRepeat
 
 
 var myApp = angular.module('myApp', []);
 myApp.controller('myAppController', ['$scope', function myAppController($scope) {
-    $scope.name = "Kuba";
-    $scope.$watch('surname', function (newVal, oldVal) {
-        console.log('New value is ' + newVal);
-    });
-    setTimeout(function () {
-        $scope.surname = "Matyka";
-        $scope.greetings = "Hello!";
-        $scope.$apply();
-    }, 1000);
+    $scope.names = ["Basia", "Kasia", "Zosia"];
 
 }]);
