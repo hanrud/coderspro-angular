bee5cf1e0a35c4f77261d705811662f00457b6e1
hania <hania.rudy@gmail.com>
2017-04-01 16:44:44 +0200

    add values to scope with ngSubmit

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myAppController', ['$scope', function myAppController($scope) {
     $scope.names = ["Basia", "Kasia", "Zosia"];
 
+    $scope.addName = function () {
+        $scope.names.push($scope.inputName)
+    }
+
 }]);
