d78959c265c4035cf647fbb65a886234059c6e6b
hania <hania.rudy@gmail.com>
2017-04-02 11:44:12 +0200

    dynamically change sorting

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+'use strict';
 var myApp = angular.module('myApp', []);
 myApp.controller('myAppCtrl', ['$scope', '$http', function myAppCtrl($scope, $http) {
+    $scope.sortField = 'population';
+    $scope.reverse = true;
+
+    $scope.sortFields = function (field) {
+        $scope.sortField = field;
+        $scope.reverse = !$scope.reverse;
+    };
+
     $http.get('countries.json').success(function (data) {
-            $scope.countries = data;
-        })
+        $scope.countries = data;
+    })
 }]);
