06deae5d80664fe98ade860bcee1b456dcd87d9e
hania <hania.rudy@gmail.com>
2017-04-08 10:42:39 +0200

    add controller to custom directive

--- app.js
+++ app.js
@@ -50,7 +50,10 @@ countryApp.directive('country', function () {
             // < - One way data binding
             country: '='
         },
-        templateUrl: 'country.html'
+        templateUrl: 'country.html',
+        controller: function ($scope, countries) {
+            console.log($scope.country);
+        }
     }
 });
 
